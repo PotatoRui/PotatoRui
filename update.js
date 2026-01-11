00ef39ccbc4b3e3bde21f232a631f59a4ac1b0f5
John Louis Domincel <johnlouisdomincel@gmail.com>
2026-01-11 10:36:17 +0800

    Update update.js

--- update.js
+++ update.js
@@ -83,7 +83,7 @@ void async function main( ) {
         "└── Databases",
         "    └── MySQL       SQLite      MongoDB         Redis",
         "```",
-        `###### This presentation is [updated](https://github.com/Asgarrrr/Asgarrrr/blob/main/update.js) automatically every 2 hours, most recently on ${ lastUpdate } ( UTC±2 )`
+        `###### This presentation is [updated](https://github.com/PotatoRui/PotatoRui/blob/main/update.js) automatically every 2 hours, most recently on ${ lastUpdate } ( UTC±2 )`
     );
 
     // ── Update README.md
